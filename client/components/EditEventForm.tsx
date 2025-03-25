55fedad9d7f62d1b18925d0d600288e0332129c5
Annalise Dawson <annaliseamydawson@gmail.com>
2025-03-26 11:23:12 +1300

    Accessibility for edit event

--- client/components/EditEventForm.tsx
+++ client/components/EditEventForm.tsx
@@ -55,7 +55,9 @@ export default function EditEventForm({
     <form onSubmit={handleSubmit} className="form">
       <input type="hidden" name="id" value="{{event.id}}" />
 
-      <label className="label">Event name</label>
+      <label className="label" htmlFor="name">
+        Event name
+      </label>
       <input
         type="text"
         id="name"
